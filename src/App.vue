d7dc7f0d8736661ad5e332711e2336b542b7f2c0
naraghababyan666 <84862784+naraghababyan666@users.noreply.github.com>
2022-11-30 01:05:42 +0400

    frontend part

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead ">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <div class="get-categories">
+          <div class="card text-white bg-primary mb-3" style="width:100%" @click="getCategoriesAPI">
+            <div class="card-header">GET - /categories</div>
+            <div class="card-body">
+              <h5 class="card-title">Получение категорий</h5>
+              <p class="card-text">Get request returned all categories from database</p>
+            </div>
+          </div>
+          <modal name="get-categories"
+                 height="auto"
+                 :scrollable="true"
+                 :adaptive="true" >
+            This is my first modal<br />
+          </modal>
+        </div>
+
+
+        <div class="card text-white bg-success mb-3" style="width:100%">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Success card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+        <div class="card text-white bg-danger mb-3" style="width:100%">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Danger card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </main>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      getCategoriesAPIData: [],
+    }
+  },
+  methods: {
+    getCategoriesAPI(){
+      console.log(this.getCategoriesAPIData)
+      this.$modal.show('get-categories')
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import vmodal from 'vue-js-modal'
+Vue.use(vmodal)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
